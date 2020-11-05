e51e7eab8281ee443672597784ec8b1e714a7d7c
Serusthe3rd <70854555+serusthe3rd@users.noreply.github.com>
2020-11-05 19:58:19 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+(hi) => {
+  console.log("hi");
+};
+hi();
+class DrumKit {
+  constructor() {
+    this.pads = document.querySelectorAll(".pad");
+    this.playButton = document.querySelector(".play");
+    this.kickAudio = document.querySelector(".kick-sound");
+    this.kickAudio = document.querySelector(".snare-sound");
+    this.kickAudio = document.querySelector(".hihat-sound");
+    this.index = 0;
+    this.bpm = 150;
+  }
+  activePad() {
+    this.classList.toggle("active");
+  }
+  repeat() {
+    let step = this.index % 8;
+    const activeBars = document.querySelectorAll(`.b${step}`);
+    this.index++;
+    console.log(step);
+  }
+  start() {
+    const interval = (60 / this.bpm) * 1000;
+    setInterval(() => {
+      this.repeat();
+    }, interval);
+  }
+}
+
+const drumKit = new DrumKit();
+
+drumKit.pads.forEach((pad) => {
+  pad.addEventListener("click", drumKit.activePad);
+});
+
+drumKit.playButton.addEventListener("click", function () {
+  drumKit.start();
+});
