9020566489a806f141cea73446ea8c8baccb6205
serusthe3rd <70854555+serusthe3rd@users.noreply.github.com>
2020-11-07 17:10:56 +0100

    added sounds
    loop and a check for active class then play the sound

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ class DrumKit {
     this.pads = document.querySelectorAll(".pad");
     this.playButton = document.querySelector(".play");
     this.kickAudio = document.querySelector(".kick-sound");
-    this.kickAudio = document.querySelector(".snare-sound");
-    this.kickAudio = document.querySelector(".hihat-sound");
+    this.snareAudio = document.querySelector(".snare-sound");
+    this.hihatAudio = document.querySelector(".hihat-sound");
     this.index = 0;
     this.bpm = 150;
   }
@@ -15,9 +15,26 @@ class DrumKit {
     let step = this.index % 8;
     const activeBars = document.querySelectorAll(`.b${step}`);
     this.index++;
-    console.log(step);
+    // Loop over the pads
+    activeBars.forEach((bar) => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+      // Check if pads are active
+      if (bar.classList.contains("active")) {
+        // Chrck each sound
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.play();
+        }
+      }
+    });
   }
   start() {
+    console.log(this);
     const interval = (60 / this.bpm) * 1000;
     setInterval(() => {
       this.repeat();
@@ -29,6 +46,9 @@ const drumKit = new DrumKit();
 
 drumKit.pads.forEach((pad) => {
   pad.addEventListener("click", drumKit.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
 });
 
 drumKit.playButton.addEventListener("click", function () {
